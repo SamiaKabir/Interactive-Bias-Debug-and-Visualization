6e3c5c9441906702dfc8e7a9795c8d30cfc1883a
Samia <samialiya212@gmail.com>
2022-04-01 13:15:32 -0400

    Lixiang's stem and lemma code integrated

--- similar_words.py
+++ similar_words.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1pBXC0PvrKhKjjVw4tVPc_mdooaDhzmw_
 """
 
-# from typing import final
 from nltk.stem import WordNetLemmatizer
 import nltk
 from collections import defaultdict
