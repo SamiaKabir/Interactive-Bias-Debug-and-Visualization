c0859e46062f93ae371e6ee0e7e8d8d0aaeb79b2
Samia <samialiya212@gmail.com>
2022-07-01 12:21:18 -0400

    all fonts increased by 1 unit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,7 @@ function App() {
              <ArrowBackIosNewIcon style={{ marginTop: '0.5vh', marginLeft: '0.6vw',marginBottom: '0.5vh', color: 'white' }} />
          </Button>
          <div className={cssStyles.biasEditorTitle}>
-            <Typography variant="subtitle1" align="right">
+            <Typography variant="body1" align="center" sx={{fontSize:'18px'}}>
               Bias Editor
             </Typography>
          </div>
